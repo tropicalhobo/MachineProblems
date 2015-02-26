9863cac481808d193b3f7d29406d8953fe04113e
tropicalhobo <jose.torres@obf.ateneo.edu>
2015-02-26 09:07:28 +0800

    Parse ascii grid header

--- gme205_TORRES_MP1.py
+++ gme205_TORRES_MP1.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 """inputDem = False
 while inputDem == False:
     try:
@@ -6,26 +7,50 @@ while inputDem == False:
         inputDem = True
     except IOError:
         print "\nCannot detect file."""
+#global variables
+hdr = {}
+val = []
 
 #translation of coordinates to center of pixel
-#def toCenter():
+"""def toCenter():
 #parse image parameters from ASCII file
-
+    
 #calculate the coordinates of the center of the pixel
+"""
 
-x = []
 #function to open and read ASCII file
 def readASCII(f):
     dem = open(f, 'r')
-    for i in dem:
+    lines = dem.readlines()
+    for i in lines: #read header and input keys and values into dictionary
+        if 'ncols' in i:
+            j = i.strip()
+            j = j.split()
+            hdr[j[0]] = int(j[1])
+        elif 'nrows' in i:
+            j = i.strip()
+            j = j.split()
+            hdr[j[0]] = int(j[1])
+        elif 'hdrllcorner' in i:
+            j = i.strip()
+            j = j.split()
+            hdr[j[0]] = float(j[1])
+        elif 'yllcorner' in i:
+            j = i.strip()
+            j = j.split()
+            hdr[j[0]] = float(j[1])
+        elif 'dx' in i:
+            j = i.strip()
+            j = j.split()
+            hdr[j[0]] = float(j[1])
+        elif 'dy' in i:
+            j = i.strip()
+            j = j.split()
+            hdr[j[0]] = float(j[1])
         
-        j=i.replace(' ','')
-        #r=j.split('')
-        k=j.strip()
-        #x.insert(0, k)
-    #if k.isdigit()==True:
-        
-        print k
+    print hdr
+    print i
+    
     dem.close()
 
 demFile = "C:\Users\G Torres\Documents\UP Diliman Stuff\GmE 205\MachineProblems\dream_dem.asc"
@@ -34,7 +59,13 @@ readASCII(demFile)
 #parse header values and pass to lowerleft-to-center pixel function
 
 #function to create and write XYZ file
-#def writeXYZ():
+"""def writeXYZ():
 
+"""
 #function to create histogram:
-#def makeHistogram():
+"""def makeHistogram():
+
+"""
+=======
+#translation of coordinates to center of pixel
+>>>>>>> parent of 6926110... ASCII to XYZ
