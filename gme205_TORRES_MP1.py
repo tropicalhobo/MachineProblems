5e51d626cbe11af6b54ebdd2818e12fb8d7eef48
tropicalhobo <jose.torres@obf.ateneo.edu>
2015-02-27 19:42:56 +0800

    Final

--- gme205_TORRES_MP1.py
+++ gme205_TORRES_MP1.py
@@ -1,22 +1,16 @@
 from math import sqrt
+from itertools import groupby
 
 #global variables
-hdr = {}
-xyz = []
-z = []
+hdr = {} #stores header keys and values
 
-#file input and read
+xyz = [] #stores xyz data for writing to file
 
-inputDem = False
-while inputDem == False:
-    try:
-        demFile = raw_input("Enter the DEM file path: ")
-        f = open(demFile, 'r')
-        inputDem = True
-    except IOError:
-        print """\nCannot detect file.
-Please type the name correctly."""
-        break
+z = [] #stores individual z values in preparation of histogram printing
+
+zInt = [] #stores integer values of z for the histogram
+
+h = '' #file path of empty xyz file
 
 #function to transform lower left coordinates to center of pixel
 def convertCoord(dx, dy): #dx and dy are coordinate pairs in the dictionary
@@ -67,56 +61,78 @@ def readASCII(f):
             j = i.strip()
             j = j.split()
             hdr[j[0]] = float(j[1])
-        elif i.isalpha() == False:#insert z values into a separate list            
+        elif i.isalpha() == False:#insert z values into the z list            
             k = i.strip()
             k = k.split()
             for o in range(len(k)):
                 z.append(k[o])
-            
+        f.close()
+        
 #function to write formatted values to an empty XYZ file
 def writeXYZ(m, t):
-    j = 0
+    counter = 0
     for i in xyz:     
-        m.writelines(xyz[j])
+        m.writelines(xyz[counter])
         #print xyz[j][0].strip(),xyz[j][1].strip(),xyz[j][2].strip()
-        j+=1
+        counter += 1
     m.close()
 
 #function to convert z values into integers:
 def convertZtoInt():
     counter = 0
-    for obs in z:
+    for obs in z[0:hdr['ncols']*hdr['nrows']]: #user can change the max value of the slice
         s = int(float(obs))
         z.remove(obs)
-        z.insert(counter, s)
-        countInt = z[0:1000].count(s)
+        zInt.insert(counter, s)        
         counter += 1
-        print s, countInt
         
-"""
 #print histogram
-def printHistogram():
-    for integer in z:
-        intCount = z.count(integer)
-        print integer, intCount*'*'
-"""       
+def printHistogram(d):
+    print "\nFrequency of values adjusted by 500."
 
-demFile = "C:\Users\G Torres\Documents\UP Diliman Stuff\GmE 205\MachineProblems\dream_dem2.asc"
+    minZ = min(d)
+    maxZ = max(d)
+    d.sort()
+    for i in range(minZ, maxZ, 1):
+        countInt = d.count(i)
+        if countInt > 500:
+            print i, countInt/500*'*', countInt/500    
 
-f = open(demFile, 'r')
-readASCII(f)
+#file input and read
+def fileInput():
+    inputDem = False
+    while inputDem == False:
+        try:
+            demFile = raw_input("\nEnter the DEM file path: ")
+            h = raw_input("\nEnter the file path of the empty xyz file: ")
+            #print "C:\Users\G Torres\Documents\UP Diliman Stuff\GmE 205\MachineProblems\dream_dem2.asc"
+            #demFile = "C:\Users\G Torres\Documents\UP Diliman Stuff\GmE 205\MachineProblems\dream_dem2.asc"
+            f = open(demFile, 'r')
+            print "\nPrinting the histogram will take some time. Please wait..."
+            readASCII(f)
+            inputDem = True
+        except IOError:
+            print """\nCannot detect files.
+Please type the name correctly."""
+
+###################################################################3
+
+fileInput()
+
 c = convertCoord(hdr['dx'], hdr['dy'])
 formatXYZ(hdr['ncols'], hdr['nrows'], hdr['dx'], hdr['dy'],
          hdr['xllcorner'], hdr['yllcorner'], z, c[0], c[1])
-f.close()
 
+#Enter the file path of the empty xyz file
 h = "C:\Users\G Torres\Documents\UP Diliman Stuff\GmE 205\MachineProblems\\new.xyz"
 
 s = open(h, 'w')
+
 writeXYZ(s, xyz)
 
 convertZtoInt()
 
-#printHistogram()
+printHistogram(zInt)
+
 
 
